53119cc31d87edf56696d15db75d9ce2215ccffb
Frank Guo <yckuo5k2b@gmail.com>
2018-06-09 10:50:40 +0800

    update index.js

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ bot.on('message', function(event) {
     }
 });
 
+setTimeout(function(){
+    var userId = '使用者 ID';
+    var sendMsg = '要發送的文字';
+    bot.push(userId,sendMsg);
+    console.log('send: '+sendMsg);
+},5000);
+
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
