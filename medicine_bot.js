1c2063351fe7238b791c06e02433e62a55eab09f
Frank Guo <yckuo5k2b@gmail.com>
2018-06-27 21:58:46 +0800

    Test Viz.js

--- medicine_bot.js
+++ medicine_bot.js
@@ -6,6 +6,25 @@ let express = require('express')
 let fs = require('fs')
 let StateMachine = require('javascript-state-machine')
 
+// These are for the visualization of the finite state machine
+const Viz = require('viz.js')
+const { Module, render } = require('viz.js/full.render.js')
+let viz = new Viz({ Module, render })
+
+viz.renderString('digraph { a -> b }')
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    // Create a new Viz instance (@see Caveats page for more info)
+    viz = new Viz({ Module, render });
+
+    // Possibly display the error
+    console.error(error);
+  })
+
+// 初始化有限狀態機
+
 // 載入題庫
 let quesBank = null
 try {  
