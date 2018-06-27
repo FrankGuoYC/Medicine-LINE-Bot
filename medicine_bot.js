e96c177a87284911b3c81f99a2965ceeda727485
Frank Guo <yckuo5k2b@gmail.com>
2018-06-28 04:57:36 +0800

    requireJs test

--- medicine_bot.js
+++ medicine_bot.js
@@ -5,6 +5,7 @@ let linebot = require('linebot')
 let express = require('express')
 let fs = require('fs')
 let StateMachine = require('javascript-state-machine')
+let rqJs = require('requireJs')
 
 // These are for the visualization of the finite state machine
 // const Viz = require('viz.js')
@@ -26,16 +27,16 @@ let StateMachine = require('javascript-state-machine')
 // 初始化有限狀態機
 // <待補上>
 
-require('jiebajs/scripts/main.js');
+rqJs.require('jiebajs/scripts/main.js');
  
 _text = "這個布丁是在無聊的世界中找尋樂趣的一種不能吃的食物，喜愛動漫畫、遊戲、程式，以及跟世間脫節的生活步調。";
  
-dict1 = require('jiebajs/scripts/data/dictionary.js');
-dict2 = require('jiebajs/scripts/data/dict_custom.js');
+dict1 = rqJs.require('jiebajs/scripts/data/dictionary.js');
+dict2 = rqJs.require('jiebajs/scripts/data/dict_custom.js');
  
 node_jieba_parsing([dict1, dict2], _text, function (_result) {
     console.log(_result.join(" "));
-    console.log("我在這裡!");
+    console.log("我在這裡!")
 });
 
 
