829e10df8de915e5103564b0a1748f71a84cb9de
Frank Guo <yckuo5k2b@gmail.com>
2018-06-29 07:32:26 +0800

    Test some bugs

--- medicine_bot.js
+++ medicine_bot.js
@@ -307,6 +307,8 @@ bot.on('message', function(event) {
             // 計算分數
             console.log("user correct Ans Num: " + user.correctAnsNum)
             console.log("user ques num: " + user.quesNum)
+            console.log(user.correctAnsNum.toFixed(2) / user.quesNum)
+            console.log(user.correctAnsNum / user.quesNum)
             user.score = Math.round( user.correctAnsNum.toFixed(2) / user.quesNum ) * 100 
             replyMsgs.push( textTp("恭喜您完成了本遊戲! 您的得分為" + user.score + "分") )
             replyMsgs.push( confirmTp("是否再玩一次呢?", ["是","好"]) )
